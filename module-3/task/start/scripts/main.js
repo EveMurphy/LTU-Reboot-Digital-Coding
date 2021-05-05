9076a7443132c41c7735a88732c554c836dad497
EveMurphy <evemurph_@outlook.com>
2021-05-05 11:17:11 +0100

    05/05/21

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -1,7 +1,39 @@
-let productName = 'Sosijs'; 
-let price = 1;
-let quantity = 69;
+let productName = 'Baked beans'; // string
+let price = 0.89; //
+let quantity = 2;
 let inStock = true;
-let discountAmount = 0.11;
+let discountAmount = 0.20;
 
-console.log( productName, price );
+// console.log( productName, price );
+
+function showProductName() {
+ console.log(productName); 
+}
+
+function totalPrice( productPrice,productQuantity ) {
+
+ console.log(productPrice * productQuantity) ;
+
+}
+
+// totalPrice(price, quantity);
+
+//function checkAvailability(productInStock) {
+//    console.log( productInStock);
+// }
+
+let checkAvailability = productInStock => console.log(productInStock );
+
+// checkAvailability(inStock);
+
+function productDiscount() {
+ if ( quantity > 1) {
+    let newPrice = (price - discountAmount * quantity);
+    console.log ( newPrice);
+ } else {
+     console.log ( price * quantity);
+    }
+
+}
+
+// productDiscount ();
