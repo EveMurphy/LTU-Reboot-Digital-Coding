669e5ce68cd4606e9f929d6060ad3b01629c6a90
EveMurphy <evemurph_@outlook.com>
2021-05-06 10:34:30 +0100

    Update main.js
    Calculator

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -37,3 +37,57 @@ function productDiscount() {
 }
 
 // productDiscount ();
+
+
+/*
+check drink
+    coke
+    lemonade
+    orangeade
+    default, what if non of those drinks are selected
+*/
+
+//function drinkOrder (drink, size) {
+
+    let drinkLow = drink.toLowerCase();
+
+    switch( drinkLow ) {
+    case 'coke':
+        console.log( drink, size ); // coke
+        break;
+    case 'lemonade':
+        console.log( drink, size ); // lemonade
+        break;
+    case 'orangeade':
+        console.log( drink, size ); // orangeade
+        break; 
+    default:
+        console.log(drink, 'is not available');
+        break;
+    }
+}
+
+//drinkOrder('coke', 'extra large');
+
+function calculator (num1 , num2, operator)
+
+switch(operator){
+    case '+':
+        console.log( '${num1} ${operator} ${num2} = ${num1 + num2}');
+        break;
+    case '-':
+        console.log( '${num1} ${operator} ${num2} = ${num1 - num2}');
+        break;
+    case '/':
+        console.log( '${num1} ${operator} ${num2} = ${num1 / num2}');
+        break;
+    case '*':
+        console.log( '${num1} ${operator} ${num2} = ${num1 * num2}');
+        break;
+    default:
+
+        break;
+    }
+
+}
+calculator (5, 5, '+');
